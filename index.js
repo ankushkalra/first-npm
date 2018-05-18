8d746c3d526a05d2e1a0df5893cb7f3e1ff412e2
Ankush Kalra <ankushkalra2@gmail.com>
2018-05-18 15:20:47 +0530

    a

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var _ = require("lodash");
 var arr = ['apple', 'banana', 'strawberry', 'banana', 'orange']
 var arr = _.concat(arr, 2, [3], [[4]]);
 
-console.log(arr);
-
 exports.printArr = () => {
 	console.log(arr);
 }
