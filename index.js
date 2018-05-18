6c35b0453c85eb20c09841f171d4a31fa695af03
Ankush Kalra <ankushkalra2@gmail.com>
2018-05-18 15:15:18 +0530

    es6 and import lodash

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var fun = require('./functions.js')
+var fun = require('./functions.js');
+var _ = require("lodash");
 
 var arr = ['apple', 'banana', 'strawberry', 'banana', 'orange']
 var arr = _.concat(arr, 2, [3], [[4]]);
 
 console.log(arr);
 
-exports.printArr = function() {
+exports.printArr = () => {
 	console.log(arr);
 }
 
-exports.printMsg = function () {
+exports.printMsg = () => {
 	console.log("Demo message");
 }
