740c4209a57dabb23ffd1724f549a511a40c3471
Ankush Kalra <ankushkalra2@gmail.com>
2018-05-18 15:08:45 +0530

    added lodash

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var fun = require('./functions.js')
 
+var arr = ['apple', 'banana', 'strawberry', 'banana', 'orange']
+var arr = _.concat(arr, 2, [3], [[4]]);
+
+console.log(arr);
+
+exports.printArr = function() {
+	console.log(arr);
+}
+
 exports.printMsg = function () {
-	console.log("This is from my Demo npm");
+	console.log("Demo message");
 }
